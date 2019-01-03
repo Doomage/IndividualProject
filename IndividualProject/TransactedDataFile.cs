1da319f9c27f573ee8bc8c273d9ccf526458072a
George Lymperopoulos <44376808+Doomage@users.noreply.github.com>
2019-01-03 21:50:40 +0200

    checking if File exists. IF not create it!

--- IndividualProject/TransactedDataFile.cs
+++ IndividualProject/TransactedDataFile.cs
@@ -6,9 +6,10 @@ namespace IndividualProject
     class TransactedDataFile
     {   
         const string path = @"C:\Users\Γιωργος\Desktop\GitHub\IndividualProject\TransactedDataFile.txt";
+
         public TransactedDataFile(string Receivername, string message, string Sendername)
         {
-
+            CheckingIfFileExists();
             using (TextWriter text = new StreamWriter(path, true))
             {
                 text.WriteLine($"User {Sendername} send to {Receivername} : {message}");
@@ -17,7 +18,7 @@ namespace IndividualProject
 
         public TransactedDataFile(string Receivername, string message, string Sendername, DateTime dateTime)
         {
-
+            CheckingIfFileExists();
             using (TextWriter text = new StreamWriter(path, true))
             {
                 text.WriteLine($"{dateTime} User {Sendername} edit {Receivername}'s message: {message}");
@@ -25,11 +26,19 @@ namespace IndividualProject
         }
         public static void DeleteMessageFile(string Receivername, string message, string Sendername, DateTime dateTime)
         {
-
+            CheckingIfFileExists();
             using (TextWriter text = new StreamWriter(path, true))
             {
                 text.WriteLine($"{dateTime} User {Sendername} delete {Receivername}'s message: {message}");
             }
+        }
+        public static void CheckingIfFileExists()
+        {      
+            if (!File.Exists(path))
+            {
+               var myfile =  File.Create(path);
+                myfile.Close();
+            }            
         }
     }
 }
